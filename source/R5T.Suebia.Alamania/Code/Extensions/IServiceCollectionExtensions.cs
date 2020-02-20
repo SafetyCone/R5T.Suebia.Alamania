6071b81d884b9b6ddb212755f282d84cb48b9532
MinexAutomation <minex.automation@gmail.com>
2020-02-20 00:55:27 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Suebia.Alamania/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Alamania;
+using R5T.Dacia;
+using R5T.Lombardy;
+
+
+namespace R5T.Suebia.Alamania
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="AlamaniaSecretsDirectoryPathProvider"/> implementation of <see cref="ISecretsDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddAlamaniaSecretsDirectoryPathProvider(this IServiceCollection services,
+            ServiceAction<IRivetOrganizationDirectoryPathProvider> addRivetOrganizationDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            services
+                .AddSingleton<ISecretsDirectoryPathProvider, AlamaniaSecretsDirectoryPathProvider>()
+                .RunServiceAction(addRivetOrganizationDirectoryPathProvider)
+                .RunServiceAction(addStringlyTypedPathOperator)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="AlamaniaSecretsDirectoryPathProvider"/> implementation of <see cref="ISecretsDirectoryPathProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static ServiceAction<ISecretsDirectoryPathProvider> AddAlamaniaSecretsDirectoryPathProviderAction(this IServiceCollection services,
+            ServiceAction<IRivetOrganizationDirectoryPathProvider> addRivetOrganizationDirectoryPathProvider,
+            ServiceAction<IStringlyTypedPathOperator> addStringlyTypedPathOperator)
+        {
+            var serviceAction = new ServiceAction<ISecretsDirectoryPathProvider>(() => services.AddAlamaniaSecretsDirectoryPathProvider(
+                addRivetOrganizationDirectoryPathProvider,
+                addStringlyTypedPathOperator));
+            return serviceAction;
+        }
+    }
+}
